090a2883fb62087e36d62991693ec0c2b64c962e
Rachel Frederickson <vampirerachel@gmail.com>
2023-09-24 13:25:02 -0400

    images displaying inside of cards

--- team-website/src/components/TeamMember.js
+++ team-website/src/components/TeamMember.js
@@ -1,15 +1,29 @@
 // TeamMember.js
 import React from 'react';
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, makeStyles } from '@mui/material';
+
+const useStyles = makeStyles({
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  media: {
+    flex: 1,
+    objectFit: 'cover', // This makes the image fit within the card without stretching
+  },
+});
 
 const TeamMember = ({ name, image, bio }) => {
+  const classes = useStyles();
   return (
-    <Card>
+    <Card className={classes.card}>
       <CardMedia
         component="img"
         height="140"
         image={image}
         alt={name}
+        style={{ maxWidth: "25%" }}
+        className={classes.media}
       />
       <CardContent>
         <Typography variant="h5">{name}</Typography>
